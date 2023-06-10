68749142e7ed9037f455dd154230f4f2130429c4
Shahzad Naeem <shahzad65naeem@gmail.com>
2023-06-10 19:09:34 +0100

    Remove binary stuff

--- arduino/data-gen/target/debug/data-gen.d
+++
@@ -1,1 +0,0 @@
-/home/shahzad/projects/bucket/arduino/data-gen/target/debug/data-gen: /home/shahzad/projects/bucket/arduino/data-gen/src/main.rs
--- arduino/data-gen/target/debug/deps/data_gen-c3e2e5453f4b09de.d
+++
@@ -1,5 +0,0 @@
-/home/shahzad/projects/bucket/arduino/data-gen/target/debug/deps/data_gen-c3e2e5453f4b09de: src/main.rs
-
-/home/shahzad/projects/bucket/arduino/data-gen/target/debug/deps/data_gen-c3e2e5453f4b09de.d: src/main.rs
-
-src/main.rs:
